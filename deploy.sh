461b709e01f27dc5ef063a5980991fbb8fac1fc6
John Buscher <john.buscher12@gmail.com>
2023-01-05 13:48:50 -0800

    minor ui and relase updates

--- deploy.sh
+++ deploy.sh
@@ -7,11 +7,9 @@ pushd ./release
 rm *.js
 rm *.json
 rm -r ./public
-cp ./server/*.js .
+cp ../server/*.js
 popd
 
-cp ./server/index.js ./release/index.js
-cp ./server/routes.js ./release/routes.js
 cp ./server/package.json ./release/package.json
 cp ./client/dist/ang-personal-website -R ./release/
 mv ./release/ang-personal-website ./release/public
