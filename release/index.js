0cc856a12c970d1a56ee23173909c274cccbce05
John Buscher <john.buscher12@gmail.com>
2023-01-07 13:31:40 -0800

    un-ignore release folder in order to allow for simple deployments from digital ocean

---
+++ release/index.js
@@ -0,0 +1,20 @@
+const express = require('express'),
+      path = require('path'),
+      cors = require('cors'),
+      routes = require('./routes');
+
+
+var isProd = process.env.NODE_ENV === 'production';
+console.log( process.env.NODE_ENV)
+const publicPath = isProd ? path.join(__dirname, 'public'):  path.join(__dirname, '..', 'client', 'dist', 'ang-personal-website');
+
+var app = express();
+app.use(cors());
+app.use(express.static(publicPath));
+
+app.use(routes)
+
+var server = app.listen( process.env.PORT || 8080, function(){
+    console.log('Listening on port ' + server.address().port);
+});
+
