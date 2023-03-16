119e7673cb4439c37057c51bc284b825486b3ef7
jbuscher <jbuscher@uw.edu>
2023-03-16 11:50:45 -0700

    cut release via mac test

--- deploy.sh
+++ deploy.sh
@@ -12,5 +12,5 @@ cp ./server/index.js ./release/index.js
 cp ./server/routes.js ./release/routes.js
 cp ./server/localDb.js ./release/localDb.js
 cp ./server/package.json ./release/package.json
-cp ./client/dist/ang-personal-website -R ./release/
+cp -r ./client/dist/ang-personal-website ./release/
 mv ./release/ang-personal-website ./release/public
