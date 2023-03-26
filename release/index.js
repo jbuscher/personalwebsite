2620507cb4651e0a399bf6f6f5caac9da3ce23c1
jbuscher <jbuscher@uw.edu>
2023-03-26 16:24:30 -0700

    release extra config

--- release/index.js
+++ release/index.js
@@ -1,15 +1,17 @@
-const express = require('express'),
-      path = require('path'),
-      cors = require('cors'),
-      routes = require('./routes');
-      Nba = require('./nba');
-
 var isProd = process.env.NODE_ENV === 'production';
 if (!isProd) {
+    console.log("loading .env")
     require('dotenv').config();
 } else {
     console.log("PRODUCTION MODE")
 }
+
+const express = require('express'),
+      path = require('path'),
+      cors = require('cors'),
+      routes = require('./routes');
+
+
 const publicPath = isProd ? path.join(__dirname, 'public'):  path.join(__dirname, '..', 'client', 'dist', 'ang-personal-website');
 
 var app = express();
